45625464b1faba21e9c61c4ee1d05e934c798c66
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-01-30 16:52:00 +0530

    Food Recommendation System

---
+++ src/main/java/pdt/std/recommendation_system/model/FoodPreference.java
@@ -0,0 +1,52 @@
+package pdt.std.recommendation_system.model;
+
+
+	public class FoodPreference {
+	    private String foodType;
+	    private String cuisine;
+	    private String dietaryRequirement;
+	    private String mealTime;
+	    private String preference;
+
+	    // Getters and Setters
+	    public String getFoodType() {
+	        return foodType;
+	    }
+
+	    public void setFoodType(String foodType) {
+	        this.foodType = foodType;
+	    }
+
+	    public String getCuisine() {
+	        return cuisine;
+	    }
+
+	    public void setCuisine(String cuisine) {
+	        this.cuisine = cuisine;
+	    }
+
+	    public String getDietaryRequirement() {
+	        return dietaryRequirement;
+	    }
+
+	    public void setDietaryRequirement(String dietaryRequirement) {
+	        this.dietaryRequirement = dietaryRequirement;
+	    }
+
+	    public String getMealTime() {
+	        return mealTime;
+	    }
+
+	    public void setMealTime(String mealTime) {
+	        this.mealTime = mealTime;
+	    }
+
+	    public String getPreference() {
+	        return preference;
+	    }
+
+	    public void setPreference(String preference) {
+	        this.preference = preference;
+	    }
+	}
+
